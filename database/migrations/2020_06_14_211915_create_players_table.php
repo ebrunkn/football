fa7617d7c95536722a3f0c95763da92d8385da01
Ebrahim Nayakkan <ebru.nkn@gmail.com>
2020-06-15 01:24:43 +0400

    init commit

---
+++ database/migrations/2020_06_14_211915_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->constrained('teams')->nullable();
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('players');
+        Schema::enableForeignKeyConstraints();
+    }
+}
